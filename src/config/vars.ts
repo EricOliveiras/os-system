515d314056379384833ba867297871cc7ffdb66d
Eric Oliveira <oiericoi@hotmail.com>
2023-05-04 18:32:57 -0300

    feat: add saltrounds in vars

--- src/config/vars.ts
+++ src/config/vars.ts
@@ -3,5 +3,10 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const port = <string>process.env.PORT;
+const saltRoundsEnv = <string>process.env.SALT_ROUNDS;
+const saltRounds = parseInt(saltRoundsEnv);
 
-export { port };
+export {
+  port,
+  saltRounds
+};
