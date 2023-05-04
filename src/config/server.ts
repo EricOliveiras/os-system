417fbdaded552252b23dcc9d13d95dffdb575937
Eric Oliveira <oiericoi@hotmail.com>
2023-05-04 18:47:12 -0300

    feat: add imports and middlewares in server file

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,5 +1,15 @@
 import express from 'express';
+import cors from 'cors';
+
+import { router } from '../routes';
+import { errorMiddleware } from '../middlewares/errorMiddleware';
 
 export const app = express();
 
 app.use(express.json());
+
+app.use(cors());
+
+app.use(router);
+
+app.use(errorMiddleware);
