b687b906ab0b7e65067a01f1387aa9a5864a513e
Eric Oliveira <oiericoi@hotmail.com>
2023-05-06 13:00:27 -0300

    refactor: add permission, permission_role

--- prisma/database/data.ts
+++ prisma/database/data.ts
@@ -1,4 +1,4 @@
-import { Role } from '@prisma/client';
+import { Permission, Permission_Role, Role } from '@prisma/client';
 
 export const roles: Role[] = [
   {
@@ -13,4 +13,84 @@ export const roles: Role[] = [
     createdAt: new Date(),
     updatedAt: new Date()
   },
+];
+
+export const permissions: Permission[] = [
+  {
+    id: 1,
+    name: 'create:user',
+    description: null,
+    created_at: new Date(),
+  },
+  {
+    id: 2,
+    name: 'read:user',
+    description: null,
+    created_at: new Date(),
+  },
+  {
+    id: 3,
+    name: 'read:user:list',
+    description: null,
+    created_at: new Date(),
+  },
+  {
+    id: 4,
+    name: 'update:user',
+    description: null,
+    created_at: new Date(),
+  },
+  {
+    id: 5,
+    name: 'delete:user',
+    description: null,
+    created_at: new Date(),
+  },
+  {
+    id: 6,
+    name: 'create:order',
+    description: null,
+    created_at: new Date(),
+  },
+  {
+    id: 7,
+    name: 'read:order',
+    description: null,
+    created_at: new Date(),
+  },
+  {
+    id: 8,
+    name: 'read:order:list',
+    description: null,
+    created_at: new Date(),
+  },
+  {
+    id: 9,
+    name: 'update:order',
+    description: null,
+    created_at: new Date(),
+  },
+  {
+    id: 10,
+    name: 'delete:order',
+    description: null,
+    created_at: new Date(),
+  }
+];
+
+export const permission_role: Permission_Role[] = [
+  { role_id: 1, permission_id: 1 },
+  { role_id: 1, permission_id: 2 },
+  { role_id: 1, permission_id: 3 },
+  { role_id: 1, permission_id: 4 },
+  { role_id: 1, permission_id: 5 },
+  { role_id: 1, permission_id: 6 },
+  { role_id: 1, permission_id: 7 },
+  { role_id: 1, permission_id: 8 },
+  { role_id: 1, permission_id: 9 },
+  { role_id: 1, permission_id: 10 },
+  { role_id: 2, permission_id: 6 },
+  { role_id: 2, permission_id: 7 },
+  { role_id: 2, permission_id: 8 },
+  { role_id: 2, permission_id: 9 }
 ];
