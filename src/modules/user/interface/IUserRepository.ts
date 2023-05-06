c7dcdfa61bec25769ddcacd612bd5f2aa70c061e
Eric Oliveira <oiericoi@hotmail.com>
2023-05-06 13:06:36 -0300

    feat: add update user password and add role

--- src/modules/user/interface/IUserRepository.ts
+++ src/modules/user/interface/IUserRepository.ts
@@ -3,10 +3,12 @@ import { User } from '@prisma/client';
 import { IUser } from './IUser';
 
 export interface IUserRespository {
-  create({ username, fullname, password, roleId }: IUser): Promise<void>;
+  create({ username, fullname, password, roleId }: IUser): Promise<User>;
   getOne(id: string): Promise<User | null>;
   getAll(): Promise<User[]>;
   update(id: string, data: IUser): Promise<void>;
   delete(id: string): Promise<void>
   getByUsername(username: string): Promise<User | null>;
+  updateUserPassword(id: string, password: string): Promise<void>;
+  addRole(id: string, roleId: number): Promise<void>;
 }
