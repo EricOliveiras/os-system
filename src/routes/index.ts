5f3b374fca5cd0008b265e82b8a617ffc3fb436d
Eric Oliveira <oiericoi@hotmail.com>
2023-05-07 16:09:44 -0300

    feat: add authenticate user route in index routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { rootRouter } from './root.routes';
 import { userRouter } from './user.routes';
+import { authRouter } from './authenticate.routes';
 
 export const router = Router();
 
+router.use('/auth', authRouter);
 router.use('/user', userRouter);
 router.use('/', rootRouter);
