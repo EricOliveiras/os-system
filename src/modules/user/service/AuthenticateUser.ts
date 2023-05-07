7f3132cc579b042721e4906ff482ba732728bc92
Eric Oliveira <oiericoi@hotmail.com>
2023-05-07 16:07:43 -0300

    feat: add user authenticate service

---
+++ src/modules/user/service/AuthenticateUser.ts
@@ -0,0 +1,42 @@
+import { sign } from 'jsonwebtoken';
+import hash from 'bcrypt';
+
+import { HttpException } from '../../../errors/HttpException';
+import { IUserAuthResponse } from '../interface/IUserAuthenticate';
+import { UserRepository } from '../repository/UserResository';
+import { jwtSecret } from '../../../config/vars';
+
+export class AuthenticateUser {
+  private repository;
+
+  constructor(userRepository: UserRepository) {
+    this.repository = userRepository;
+  }
+
+  public async execute(username: string, password: string): Promise<IUserAuthResponse> {
+    const user = await this.repository.getByUsername(username.toLowerCase());
+
+    if (!user || !hash.compareSync(password, user.password)) {
+      throw new HttpException(401, 'Ivalid credentials.');
+    }
+
+    const permissions = user.User_Role.map((i) => {
+      return i.Role.Permission_Role.map((i) => i.Permission.name);
+    }).flat(1);
+
+    const token = sign(
+      { user_id: user.id, permissions: permissions },
+      jwtSecret,
+      { expiresIn: '7d' },
+    );
+
+    return {
+      user: {
+        id: user.id,
+        username: user.username
+      },
+      token
+    };
+  }
+}
+
