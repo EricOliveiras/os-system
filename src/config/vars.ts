1877a7b9f14c42a005197e51d2d33d4f154f6ffc
Eric Oliveira <oiericoi@hotmail.com>
2023-05-07 15:46:41 -0300

    feat: add jwt secret var

--- src/config/vars.ts
+++ src/config/vars.ts
@@ -5,8 +5,10 @@ dotenv.config();
 const port = <string>process.env.PORT;
 const saltRoundsEnv = <string>process.env.SALT_ROUNDS;
 const saltRounds = parseInt(saltRoundsEnv);
+const jwtSecret = <string>process.env.JWT_SECRET;
 
 export {
   port,
-  saltRounds
+  saltRounds,
+  jwtSecret
 };
