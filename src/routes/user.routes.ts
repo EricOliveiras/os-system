ea0b3f028935216aa3c98154fd0f5d06243b42e4
Eric Oliveira <oiericoi@hotmail.com>
2023-05-08 10:53:32 -0300

    feat: add authentication and authorization in user routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,11 +1,42 @@
 import { Router } from 'express';
 import UserController from '../modules/user/controller/UserController';
+import { canRequest } from '../middlewares/permissionsMiddleware';
+import { authenticated } from '../middlewares/authenticateMiddleware';
 
 export const userRouter = Router();
 
-userRouter.post('/create', UserController.createUser);
-userRouter.get('/list', UserController.getAll);
-userRouter.get('/search/:id', UserController.getUser);
-userRouter.put('/update/:id', UserController.update);
-userRouter.put('/update/password/:id', UserController.updatePassword);
-userRouter.delete('/delete/:id', UserController.delete);
+userRouter.post('/create',
+  authenticated,
+  canRequest('create:user'),
+  UserController.createUser
+);
+
+userRouter.get('/list',
+  authenticated,
+  canRequest('read:user:list'),
+  UserController.getAll
+);
+
+userRouter.get('/search/:id',
+  authenticated,
+  canRequest('read:user'),
+  UserController.getUser
+);
+
+userRouter.put('/update/:id',
+  authenticated,
+  canRequest('update:user'),
+  UserController.update
+);
+
+userRouter.put('/update/password/:id',
+  authenticated,
+  canRequest('update:user'),
+  UserController.updatePassword
+);
+
+userRouter.delete('/delete/:id',
+  authenticated,
+  canRequest('delete:user'),
+  UserController.delete
+);
