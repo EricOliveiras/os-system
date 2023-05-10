fc945a40c41de22873051f90c5be0d32ab0b3b21
Eric Oliveira <oiericoi@hotmail.com>
2023-05-10 11:31:02 -0300

    feat: add service order repository

---
+++ src/modules/serviceOrder/repository/ServiceOrderRepository.ts
@@ -0,0 +1,46 @@
+import { serviceOrder } from '@prisma/client';
+
+import { IServiceOrderRepository } from '../interface/IServiceOrderRepository';
+import { IServiceOrderRequest } from '../interface/IServiceOrderRequest';
+import { prisma as db } from '../../../config/prisma';
+
+export class ServiceOrderRepository implements IServiceOrderRepository {
+  public async create({ userId, requester, problemDescription }: IServiceOrderRequest): Promise<serviceOrder> {
+    return await db.serviceOrder.create({
+      data: {
+        userId,
+        requester,
+        problemDescription
+      }
+    });
+  }
+
+  public async getOne(id: string): Promise<serviceOrder | null> {
+    return await db.serviceOrder.findUnique({
+      where: { id }
+    });
+  }
+
+  public async getAll(): Promise<serviceOrder[]> {
+    return await db.serviceOrder.findMany();
+  }
+
+  public async getAllByUser(userId: string): Promise<serviceOrder[]> {
+    return await db.serviceOrder.findMany({
+      where: { userId: userId }
+    });
+  }
+
+  public async update(id: string, serviceOrder: Partial<serviceOrder>): Promise<serviceOrder> {
+    return await db.serviceOrder.update({
+      where: { id },
+      data: serviceOrder
+    });
+  }
+
+  public async delete(id: string): Promise<void> {
+    await db.serviceOrder.delete({
+      where: { id }
+    });
+  }
+}
