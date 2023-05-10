5ab3d924b53f34fe8f7fba816bf1c6729431e8d4
Eric Oliveira <oiericoi@hotmail.com>
2023-05-10 11:34:12 -0300

    feat: add service order routes in index route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,9 +3,11 @@ import { Router } from 'express';
 import { rootRouter } from './root.routes';
 import { userRouter } from './user.routes';
 import { authRouter } from './authenticate.routes';
+import { serviceOrder } from './serviceOrder.routes';
 
 export const router = Router();
 
 router.use('/auth', authRouter);
 router.use('/user', userRouter);
+router.use('/service-order', serviceOrder);
 router.use('/', rootRouter);
